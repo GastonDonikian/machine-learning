de7e53c8067566665c30dfd597bdc1b1765318a3
Gaston Donikian <gdonikian@itba.edu.ar>
2023-04-08 12:19:00 -0300

    Debugged ejercicio2.py Co-authored-by: Natu <nlilienthal@itba.edu.ar>

--- TP1/ejercicio2.py
+++ TP1/ejercicio2.py
@@ -12,62 +12,64 @@ random.seed(SEED)
 categories = ['Nacional', 'Destacadas', 'Deportes', 'Salud']
 most_common = 1000
 
+
 def filterUselessWordsAndTokenized(data):
-    articles = ["la","lo","los","las","el","ella","ellos","una","unos","un","y","al","del","le"]
-    prepositions = ["a", "ante", "bajo", "cabe", "con", "contra", 
-                    "de", "desde", "durante", "en","entre", "hacia",
-                    "hasta", "mediante", "para", "por", "según", 
+    articles = ["la", "lo", "los", "las", "el", "ella", "ellos", "una", "unos", "un", "y", "al", "del", "le"]
+    prepositions = ["a", "ante", "bajo", "cabe", "con", "contra",
+                    "de", "desde", "durante", "en", "entre", "hacia",
+                    "hasta", "mediante", "para", "por", "según",
                     "sin", "so", "sobre", "tras"]
-    extra = ["que", "su", "se", "fue", "como",'|']
+    extra = ["que", "su", "se", "fue", "como", '|']
     data = data.lower()
-    punctuations = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '¡', '¿']
-    tokenized_data = re.split('\W+',data)
+    punctuations = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>',
+                    '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '¡', '¿']
+    tokenized_data = re.split('\W+', data)
     return list(filter(lambda x: x.lower() not in articles and
-                        x.lower() not in prepositions and
-                        x.lower() not in extra and
-                        x.lower() not in punctuations,tokenized_data))
-    
+                                 x.lower() not in prepositions and
+                                 x.lower() not in extra and
+                                 x.lower() not in punctuations, tokenized_data))
+
+
 def count_words_in_array(title, category):
     word_count = 0
     for word in title.split():
         word_count += category.count(word)
     return word_count
 
+
 def news_filter(data):
-   
     dataFilter = []
     for l in data.values:
         if l[2] in categories:
             dataFilter.append(l)
-    
-   # train, test = train_test_split(dataFilter, test_size=0.3) #Preg si se puede usar esta lib
-    
+
+    # train, test = train_test_split(dataFilter, test_size=0.3) #Preg si se puede usar esta lib
+
     train = data.to_numpy()
-    nacional= list(filter(lambda x: x[-1] == 'Nacional',train))
-    destacadas= list(filter(lambda x: x[-1] == 'Destacadas',train))
-    deportes= list(filter(lambda x: x[-1] == 'Deportes',train))
-    salud = list(filter(lambda x: x[-1] == 'Salud',train))
-    
+    nacional = list(filter(lambda x: x[-1] == 'Nacional', train))
+    destacadas = list(filter(lambda x: x[-1] == 'Destacadas', train))
+    deportes = list(filter(lambda x: x[-1] == 'Deportes', train))
+    salud = list(filter(lambda x: x[-1] == 'Salud', train))
 
     nacional_filter = []
     destacadas_filter = []
     deportes_filter = []
     salud_filter = []
-    
+
     for news in list(nacional):
         nacional_filter.append(filterUselessWordsAndTokenized(news[0]))
 
     for news in list(destacadas):
         destacadas_filter.append(filterUselessWordsAndTokenized(news[0]))
-    
+
     for news in list(deportes):
         deportes_filter.append(filterUselessWordsAndTokenized(news[0]))
 
     for news in list(salud):
         salud_filter.append(filterUselessWordsAndTokenized(news[0]))
-    
 
     return salud_filter, destacadas_filter, deportes_filter, nacional_filter
+
 
 def most_commons_words_list(nacional_filter, destacadas_filter, salud_filter, deportes_filter):
     most_common_words_nacional = most_commons(nacional_filter)
@@ -77,32 +79,32 @@ def most_commons_words_list(nacional_filter, destacadas_filter, salud_filter, de
 
     return most_common_words_deportes, most_common_words_destacadas, most_common_words_nacional, most_common_words_salud
 
+
 def most_commons(words):
     word_counter = Counter()
     for n in words:
         word_counter.update(n)
-    
+
     most_common_words = [word for word, count in word_counter.most_common(most_common)]
 
     return most_common_words
 
-def map_frecuency_words(words): 
+
+def map_frecuency_words(words):
     dictonary = {}
     count = 0
     for t in words:
         for w in t:
             count += 1
-            
+
     for t in words:
         for w in t:
-            dictonary[w] = dictonary.setdefault(w, 0) + (1/len(words)) 
-    
+            dictonary[w] = dictonary.setdefault(w, 0) + (1 / len(words))
+
     return dictonary
 
-def classify_input(title_input,salud_filter, destacadas_filter, deportes_filter, nacional_filter):
-
-   
-    #most_common_words_deportes, most_common_words_destacadas, most_common_words_nacional, most_common_words_salud = most_commons_words_list(nacional_filter, destacadas_filter, salud_filter, deportes_filter)
+def classify_input(title_input, salud_filter, destacadas_filter, deportes_filter, nacional_filter):
+    # most_common_words_deportes, most_common_words_destacadas, most_common_words_nacional, most_common_words_salud = most_commons_words_list(nacional_filter, destacadas_filter, salud_filter, deportes_filter)
     dict_deportes = map_frecuency_words(deportes_filter)
     dict_destacadas = map_frecuency_words(destacadas_filter)
     dict_nacional = map_frecuency_words(nacional_filter)
@@ -113,54 +115,45 @@ def classify_input(title_input,salud_filter, destacadas_filter, deportes_filter,
     len_nacional = len(nacional_filter)
     len_salud = len(salud_filter)
     count = len_deportes + len_destacadas + len_nacional + len_salud
-    
+
     probability_deportes = len_deportes / count
     probability_destacadas = len_destacadas / count
     probability_nacional = len_nacional / count
     probability_salud = len_salud / count
-    
-    
-    
+
     tokenized_data = title_input.split(' ')
 
-
     for word in tokenized_data:
-        probability_deportes *=  (( dict_deportes.get(word, 0) + 1)/4)
-        probability_destacadas *=  ((dict_destacadas.get(word,  0) + 1)/4)
-        probability_nacional *=  ((dict_nacional.get(word,  0) + 1)/4)
-        probability_salud *=  ((dict_salud.get(word,  0) + 1)/4)
+        probability_deportes *= ((dict_deportes.get(word, 0) + 1) / 4)
+        probability_destacadas *= ((dict_destacadas.get(word, 0) + 1) / 4)
+        probability_nacional *= ((dict_nacional.get(word, 0) + 1) / 4)
+        probability_salud *= ((dict_salud.get(word, 0) + 1) / 4)
 
     dict = {}
-    dict["deportes"] = probability_deportes
-    dict["destacadas"] = probability_destacadas
-    dict["nacional"] = probability_nacional
-    dict["salud"] = probability_salud
+    dict["Deportes"] = probability_deportes
+    dict["Destacadas"] = probability_destacadas
+    dict["Nacional"] = probability_nacional
+    dict["Salud"] = probability_salud
 
     return dict
 
-    
-
-
 
 if __name__ == "__main__":
-    data = pd.read_excel('./resources/Noticias_argentinas.xlsx')
-    data = pd.DataFrame(data, columns = ['titular', 'fuente', 'categoria']) 
+    data = pd.read_excel('./resources/Noticias_argentinas.xlsx', nrows=20000)
+    data = pd.DataFrame(data, columns=['titular', 'fuente', 'categoria'])
 
-    training, test = metrics.cross_validation(data,10)
+    training, test = metrics.cross_validation(data, 10)
 
     salud_filter, destacadas_filter, deportes_filter, nacional_filter = news_filter(training)
-   
+
     expected = training['categoria'].to_numpy()
-    
 
     predicted = []
-    
+
     for i in training['titular'].to_numpy():
-        dict = classify_input(i,salud_filter, destacadas_filter, deportes_filter, nacional_filter)
-        category = max(dict, key=dict.get)      
+        dict = classify_input(i, salud_filter, destacadas_filter, deportes_filter, nacional_filter)
+        category = max(dict, key=dict.get)
         predicted.append(category)
 
-
-
-    metrics.confusion_matrix(4,expected,predicted)
-    #classify_input("Trabajadores del Buenos Aires Design cortan la avenida Libertador por el cierre del centro comercial")
+    metrics.confusion_matrix(categories, expected, predicted)
+    # classify_input("Trabajadores del Buenos Aires Design cortan la avenida Libertador por el cierre del centro comercial")
--- TP1/metrics.py
+++ TP1/metrics.py
@@ -7,34 +7,36 @@ iterations = 10
 SEED = 2023
 random.seed(SEED)
 
-def cross_validation(dataset,  k):
-    n = int(len(dataset)/k)
-    dataset.sample(frac=1) #ver si mod o si devuelve
-    
-    test = dataset.tail(-n)
-    
-    return dataset, test
-    
-    
+
+def cross_validation(dataset, k):
+    n = int(len(dataset) / k)
+    dataset = dataset.sample(frac=1).reset_index(drop=True)
+    for i in range(k):
+        test_indices = np.random.choice(len(dataset), n, replace=False)
+        test = dataset.iloc[test_indices].reset_index(drop=True)
+        train = dataset.drop(test_indices).reset_index(drop=True)
+    return train, test
 
 
 def confusion_matrix(classes, predicted, expected):
-    matrix = np.zeros((len(classes)+1, len(classes)+1))
-    for i,j  in expected, predicted:
+    print('Confusion matrix!')
+    matrix = np.zeros((len(classes) + 1, len(classes) + 1))
+    for i, j in zip(expected, predicted):
+        print(f"Expected: {i}, Predicted: {j}")
         matrix[get_index(i)][get_index(j)] += 1
     print(matrix)
     return matrix
 
+
 def get_index(category):
-    if category == 'deportes':
+    if category == 'Deportes':
         return 0
-    if category == 'destacadas':
+    if category == 'Destacadas':
         return 1
-    if category == 'nacional':
+    if category == 'Nacional':
         return 2
-    if category == 'salud':
+    if category == 'Salud':
         return 3
-    
 
 
 def accuracy(network, dataset, epsilon=0.1):
@@ -43,9 +45,9 @@ def accuracy(network, dataset, epsilon=0.1):
     for i in range(len(dataset)):
         expected = dataset[i][1][0]
         o = network.feedforward(dataset[i][0])
-        if (np.abs(expected-o) <= epsilon):
+        if (np.abs(expected - o) <= epsilon):
             good += 1
         else:
             bad += 1
-    p = good/(good + bad)
+    p = good / (good + bad)
     return p
