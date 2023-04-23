6296a2f21dd0e8fdd289f2189b1092e7b46e05a5
Natu <nlilienthal@itba.edu.ar>
2023-04-23 17:16:26 -0300

    continue with ex1, in progress
    Co-authored-by: Agustin Spitzner <aspitzner01@gmail.com>

--- TP2/ejercicio1.py
+++ TP2/ejercicio1.py
@@ -8,6 +8,7 @@ import numpy as np
 import math
 from collections import defaultdict
 
+creditability = "Creditability"
 
 def preprocessing(data_frame, name, parts):
     data = data_frame[name].to_numpy()
@@ -21,10 +22,10 @@ def preprocessing(data_frame, name, parts):
 
 
 class Node:
-    def __init__(self, desc_list, value, data_filter):
+    def __init__(self, desc_list, value, entropy):
         self.descendants = desc_list
+        self.entropy = entropy
         self.value = value
-        self.data_filter = data_filter ##Esta es la data filtrada con todos los registros que tengan al padre y a la data filtrada de sus padres
 
     def get_descendant(self, i: int):
         return self.descendants[i]
@@ -34,52 +35,111 @@ class Node:
 
     def get_value(self):
         return self.value
+    
+    def get_entropy(self):
+        return self.entropy
 
 
 def entropy(probabilities):
     en = 0
     for p in probabilities:
-        en -= p * math.log2(p)
+        if p != 0:
+            en -= p * math.log2(p)
     return en
     
 
-def gain(relative_probs_father_value,values,probabilities_father,probabilities_father_value):
-    g = entropy(probabilities_father)
+def gain(relative_probs_value, values,g):
     
-    for idx in range(values):
-        g -= relative_probs_father_value[idx] *  entropy(probabilities_father_value[idx])
+    print("relative props")
+    print(relative_probs_value)
+    entropies = []
+    for value in values:
+        e= entropy(relative_probs_value.get(value)[1:])
+        g -= relative_probs_value.get(value)[0] *  e
+        entropies.append(e)
     
-    return g
+    return g, entropies
     
 
-def set_probabilities(training_set,attr_name,father,values):
-    relative_probs_father_value = []
-    son_registers = []
-    #probabilities_father_value = []
-    #father_registers = filter()
-
-    for v in values:
-        relative_probs_father_value.append((father.registers.loc[father.registers[attr_name] == v].size[0]) /len(father.registers))
-    return relative_probs_father_value
+def set_probabilities(training,attr_name,values):
+    relative_probs_value = {}
+    for value in values:
+        training_filter = training.loc[training[attr_name] == value]
+        relative_probs_value[value] = []
+        size = training.shape[0]
+        if size == 0:
+            relative_probs_value[value].append(0)
+        else:
+            relative_probs_value[value].append(((training_filter).shape[0])/size) 
+        crediatability_filter = training_filter.loc[training[creditability] == 0]
+        size = training_filter.shape[0]
+        if size == 0:
+            relative_probs_value[value].append(0)
+            relative_probs_value[value].append(0)
+        else: 
+            relative_probs_value[value].append(crediatability_filter.shape[0]/size)
+            relative_probs_value[value].append((training_filter.shape[0]-crediatability_filter.shape[0])/size)
+    return relative_probs_value
 
 def get_criteria_probabilities(data_array, value):
     return data_array.count(value)/len(data_array)
 
 
-def get_max_gain(training_set, attributes, values, father):
+def get_max_gain(training, attributes, values, father):
     max_gain = 0
+    attribute_max_gain = ""
+    g = 0
+    entropies = []
+    if father is not None:
+        g = father.get_entropy()
+    else:
+        probabilities = []
+        crediatability_filter = training.loc[training[creditability] == 0]
+        probabilities.append(crediatability_filter.shape[0]/training.shape[0])
+        probabilities.append((training.shape[0]-crediatability_filter.shape[0])/training.shape[0])
+        g = entropy(probabilities)
+
+
     for attribute in attributes:
-        relative_probs_father_value,probabilities_father_value = set_probabilities(training_set,attribute,data,father, values[attribute])
-        g = gain(relative_probs_father_value,probabilities_father_value)
-        max_gain = max(max_gain,max)
+        relative_probs_value = set_probabilities(training,attribute, values[attribute])
+        g, e = gain(relative_probs_value,values[attribute], g)
+        print(attribute)
+        print(max_gain)
+        print(g)
+        if max_gain < g:
+            max_gain = g
+            attribute_max_gain = attribute
+            entropies = e
 
-    return max(map(lambda x: gain(),training_set))
+    print("ATTRIBUTE MAX GAIN " + attribute_max_gain)
+    return attribute_max_gain, entropies
 
 
-def id3(training_set, attributes, values, node):
-    atribute_max_gain = get_max_gain(training_set, attributes, values, None)
+def id3(training, attributes, values, father):
+       
+    if len(attributes) == 0:
+        return father
     
+    
+    attribute_max_gain, entropies = get_max_gain(training, attributes, values, father)
 
+    print("ID3")
+    print(attribute_max_gain)
+    print(entropies) ##ta dando raro esto
+    for idx,value in enumerate(values[attribute_max_gain]):
+        new_node = Node([],value,entropies[idx])
+        if father is not None:
+            father.descendants.append(new_node)
+        training_filter = training.loc[training[attribute_max_gain] == value]
+        new_attributes = attributes.copy()
+        new_attributes.remove(attribute_max_gain)
+        new_val = values.copy()
+        new_val.pop(attribute_max_gain)
+        id3(training_filter,new_attributes,new_val,new_node)
+
+    return father
+
+    
     ## nodo vacio
     ##ganacia para los attributos set
     ##> ganancia --> lo agregamos al arbol
@@ -112,6 +172,9 @@ if __name__ == "__main__":
     data = replaces_process_data(data, 'Credit Amount', 3)
     data = replaces_process_data(data, 'Age (years)', 3)
 
+    attributes.remove(creditability)
+    print(attributes)
+    print("------")
     values_per_atr = {}
     for atr in attributes :
         values_per_atr[atr] = data[atr].unique()
@@ -123,4 +186,7 @@ if __name__ == "__main__":
     for j in range(1, 10):
          training = pd.concat([training, df_list[j]], axis=0)
 
+    print("MAIN")
+
+    print(id3(data,attributes,values_per_atr,None))
     
