bdf7b1735abf52f4f9a1445ad32193c240bee5b8
Natu <nlilienthal@itba.edu.ar>
2023-06-10 20:29:20 -0300

    fix hierarchical_clustering

--- TP4/algorithms/hierarchical_clustering.py
+++ TP4/algorithms/hierarchical_clustering.py
@@ -24,44 +24,42 @@ def _get_distance_matrix(points):
             distance_matrix[j][i] = distance_matrix[i][j]
     return distance_matrix
 
+def _calculate_distance_clusters_min_distance(distance_matrix,cluster1, cluster2):
+    # Mapeo todos los pares de distancias a un array, y hago el minimo
+    distance = np.min([distance_matrix[p1, p2] for p1 in cluster1 for p2 in cluster2])
+    return distance
 
-def hierarchical_clustering(points, number_of_clusters):
+def hierarchical_clustering(points,calculate_distance='min'):
     distance_matrix = _get_distance_matrix(points=points)
+    final_clusters = []
     # Cada cluster es un array con un unico elemento, ahora tengo que mergear
     clusters = [[i] for i in range(len(points))]
-    if number_of_clusters < 1:
-        raise ValueError("Number of clusters can't be less than 1.")
-    while len(clusters) > number_of_clusters:
+    while len(clusters) > 1:
         min_distance = np.inf
         merge_indices = None
         for i in range(len(clusters)):
             for j in range(i + 1, len(clusters)):
-                # Mapeo todos los pares de distancias a un array, y hago el minimo
-                distance = np.min([distance_matrix[p1, p2] for p1 in clusters[i] for p2 in clusters[j]])
+                distance = calculate_distance_clusters.get(calculate_distance)(distance_matrix,clusters[i],clusters[j])
                 if distance < min_distance:
                     min_distance = distance
                     # me guardo la tupla de indices
                     merge_indices = (i, j)
 
         # Merge the closest clusters
+        
         merged_cluster = clusters[merge_indices[0]] + clusters[merge_indices[1]]
         clusters = [c for idx, c in enumerate(clusters) if idx not in merge_indices]
         clusters.append(merged_cluster)
-
-    final_clusters = []
-    for cluster in clusters:
-        points_in_cluster = [points[i] for i in cluster]
-        final_clusters.append(points_in_cluster)
-
+        final_clusters.append(merged_cluster)
+   
     return final_clusters
 
-
+calculate_distance_clusters = { 'min': _calculate_distance_clusters_min_distance}
 
 
 def ejemplo():
     data = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
-    num_clusters = 4
-    clusters = hierarchical_clustering(data, num_clusters)
+    clusters = hierarchical_clustering(data)
     print("Final clusters:", clusters)
 def main():
     ejemplo()
