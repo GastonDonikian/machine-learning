860457b2b217f0e6e9f1bbf177dc5a8724f4707b
aspitzner <aspitzner01@gmail.com>
2023-06-10 19:47:15 -0300

    Add preproccessing

--- TP4/ejercicio1.py
+++ TP4/ejercicio1.py
@@ -1,5 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
+
+def date_to_int(d):
+    return str(d)
+
+def preprocess_csv():
+    data_frame = pd.read_csv('./resources/movie_data.csv', delimiter=';')
+    column_titles = data_frame.columns.tolist()
+    data_frame = data_frame.iloc[1:]
+
+    data_frame = data_frame.dropna()
+    data_frame.drop('imdb_id', axis=1, inplace=True)
+    data_frame.drop('original_title', axis=1, inplace=True)
+    data_frame.drop('overview', axis=1, inplace=True)
+    data_frame.drop('release_date', axis=1, inplace=True)
+
+    genres = data_frame.pop("genres")
+    normalized_df=(data_frame-data_frame.mean())/data_frame.std()
+
+    for column in data_frame:
+        col = data_frame[column]
+        data_frame[column] = (col-col.mean())/col.std()
+
+    data = data_frame.to_numpy()
+    
+    
+    
 
 
 def ejercicio_a():
@@ -20,7 +47,8 @@ def ejercicio_a():
 
 
 def main():
-    ejercicio_a()
+    #ejercicio_a()
+    preprocess_csv()
 
 
 if __name__ == "__main__":
