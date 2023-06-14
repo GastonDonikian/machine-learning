d27e87698f162f1e3feb5c22a7583babf68ba97f
aspitzner <aspitzner01@gmail.com>
2023-06-13 21:59:59 -0300

    Changes to preprocess

--- TP4/preprocess.py
+++ TP4/preprocess.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from algorithms.kohonen_som import predict, kohonen_som
 import algorithms.hierarchical_clustering as hc
-from algorithms.k_medias import k_means
+from algorithms.k_medias import k_means, get_cluster_by_point
 import numpy as np
 import metrics
 import pickle
-import copy
 
 
 
@@ -24,6 +23,10 @@ def preprocess_csv():
                         'Horror': 8, 'Romance': 9, 'Mystery': 10, 'Thriller': 11, 'Documentary': 12, 'Animation': 13,
                         'Family': 14, 'History': 15, 'War': 16, 'Western': 17, 'Music': 18, 'TV Movie': 19,
                         'Foreign': 20}
+    
+    valid_categories = ['Action','Comedy','Drama']
+    data_frame = data_frame[data_frame['genres'].isin(valid_categories)] 
+    
     for k in genre_preprocess:
         data_frame['genres'] = data_frame['genres'].replace([k], [int(genre_preprocess[k])])
     data_frame = data_frame.dropna()
@@ -31,11 +34,14 @@ def preprocess_csv():
     data_frame.drop('original_title', axis=1, inplace=True)
     data_frame.drop('overview', axis=1, inplace=True)
     data_frame.drop('release_date', axis=1, inplace=True)
-
+    print("Old genres")
+    print(data_frame['genres'])
     for column in data_frame:
         col = data_frame[column]
         data_frame[column] = (col - col.mean()) / col.std()
-
+    print("Normalized genres")
+    print(data_frame['genres'])
+    #print(data_frame)
     data = data_frame.to_numpy()
     return data
 
@@ -55,6 +61,22 @@ def ejercicio_a():
         plt.ylabel('Values')
     # Display the boxplots
     plt.show()
+
+
+def hierarchical_graph(clusters):
+    #print("HOLA")
+    print(clusters)
+
+def ej1_hierarchical():
+    data = preprocess_csv()
+    cut_length = len(data)//400
+
+    cut_array = data[:cut_length]
+    print(len(cut_array))
+    print("in")
+    clusters = hc.hierarchical_clustering(cut_array)
+    print("out")
+    hierarchical_graph(clusters)
 
 
 def save_var(var):
@@ -85,11 +107,12 @@ def ej1_kohonen():
     print(len(test))
     print("training")
     print(len(training))
+    
 
     mean_distances_by_k = []
     epochs = 100
     epochs_list = np.array(range(epochs))
-    for ki in [7]:
+    for ki in [3]:
         # k=7
         k = ki
         rows = k
@@ -102,7 +125,15 @@ def ej1_kohonen():
         mean_distances_by_k.append(mean_distances_per_epoch)
         # plt.plot(epochs_list, mean_distances_per_epoch, label="k=" +str(k))
 
-    # predict(example=data[0], trained_matrix=trained_matrix, popularity_matrix=popularity_matrix)
+    categories = np.zeros((k,k,3))
+    for t in test:
+        category = t[1]
+        i,j,min_distance = predict(example=t, trained_matrix=trained_matrix, popularity_matrix=popularity_matrix)
+        categories[i][j] += 1
+    for t in test:
+        category = t[1]
+    print(categories)
+
     # save_var(mean_distances_by_k)
 
     # print("Popularity matrix")
@@ -145,10 +176,45 @@ def ej1_k_medias():
     # print(centroids)
     # print("Clusters")
     # print(clusters)
+    norm_values_to_categories = {'-1.273803': 2, '-0.081453': 3, '1.110897': 4}
+    cats = []
+    for i in range(0,k):
+        cats.append({})
+        #print("Len cluster")
+        #print(len(clusters[i]))
+        for x in clusters[i]:
+            if str(round(x[1],6)) not in cats[i]:
+                cats[i][str(round(x[1],6))] = 1
+            else:
+                cats[i][str(round(x[1],6))] = cats[i][str(round(x[1],6))] + 1
+    print(cats)
+    predominant_category = []
+    for i in range(0,k):
+        predominant_category.append(norm_values_to_categories[max(cats[i], key=cats[i].get)])
+    print("Predominant category by cluster")
+    print(predominant_category)
+
+    mat = np.zeros((3,3))
+
+    for t in test:
+        genre = norm_values_to_categories[str(round(t[1],6))]
+        categ = predominant_category[get_cluster_by_point(point=t,centroids=centroids)]
+        mat[genre-2][categ-2] = mat[genre-2][categ-2] + 1
+    print("Matrix")
+    print(mat)
+    heatmap = plt.pcolor(mat)
+    plt.colorbar()
+    plt.show()
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    #main()
+    # main()
     #ej1_kohonen()
     ej1_k_medias()
+    #ej1_hierarchical()
     #ej1_k_medias()
