5ce50555597b3faeee3b581f0231d313be0becf8
Natu <nlilienthal@itba.edu.ar>
2023-07-09 20:40:12 -0300

    add heatmaps to dbscan

--- TPF/preprocess.py
+++ TPF/preprocess.py
@@ -3,24 +3,52 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+from pandas.plotting import parallel_coordinates
+import seaborn as sns
 
 def analyze(x):
     #function fit dbscan
     print("Started Analyzing")
     # cluster the data into five clusters
-    dbscan = DBSCAN(eps = 0.3, min_samples = 2).fit(x) # fitting the model
-    labels = dbscan.labels_ # getting the labels
-    print("DBSCAN Result")
-    print(labels)
+    dbscan = DBSCAN(eps = 0.3, min_samples = 4).fit(x) # fitting the model
+    #labels = dbscan.labels_ # getting the labels
+    #print("DBSCAN Result")
+    #print(labels)
 
     column_names = x.columns.tolist()
     print(column_names)
+
+    clusters = dbscan.fit_predict(x)
+
+    # Print the clusters
+    #print("Data Points:\n", x)
+    #print("Cluster Labels:\n", clusters)
+
+
+
+    for column in x.columns:
+        plt.figure()
+        plt.title(f'Heatmap of {column} by Cluster')
+        
+        # Create a copy of the dataframe and add the cluster labels
+        ordered_df = x.copy()
+        ordered_df['Cluster Labels'] = clusters
+        
+        # Sort the dataframe by the variable column within each cluster
+        ordered_df = ordered_df.sort_values(by=[column])
+        sns.heatmap(ordered_df[[column]], cmap='plasma', cbar=False)
+        #plt.xlabel(clusters)
+        y_labels = [round(value, 2) for value in ordered_df[column]]
+        print(y_labels)
+        #plt.ylabel(y_labels)
+        plt.yticks(ticks=range(len(ordered_df)), labels=y_labels)
+        plt.show()
     # Plot the clusters
     #plt.scatter(x[:, 0], x[:,1], c = labels, cmap= "plasma") # plotting the clusters
-    plt.scatter(x[column_names[0]], x[column_names[1]], c = labels, cmap= "plasma") # plotting the clusters
-    plt.xlabel("Avg Sleep Mins") # X-axis label
-    plt.ylabel("Avg Daily Steps") # Y-axis label
-    plt.show() # showing the plot
+    #plt.scatter(x[column_names[0]], x[column_names[1]], c = labels, cmap= "plasma") # plotting the clusters
+    #plt.xlabel("Avg Sleep Mins") # X-axis label
+    #plt.ylabel("Avg Daily Steps") # Y-axis label
+    #plt.show() # showing the plot
 
 
 def preprocess_tables():
